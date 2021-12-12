835569e189116d3c4d87237694f7334f82e2e518
moon man <humanimations10@gmail.com>
2021-12-12 16:42:06 +1100

    hi

--- day8/part2.cpp
+++ day8/part2.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-
-
 int main(){
     vector<vector<string>> data;
 
@@ -22,38 +20,78 @@ int main(){
         for(int i=0;i<10;i++){
             t = str.find(" ", ot);
             data.back().push_back( str.substr(ot, t - ot) );
-            //printf("!%s!", str.substr(ot, t - ot).c_str());
             ot = t + 1;
         }
 
         ot += 2;
 
-        //printf("|");
         for(int i=0;i<4;i++){
             t = str.find(" ", ot);
             data.back().push_back( str.substr(ot, t - ot) );
             //printf("!%s!", str.substr(ot, t - ot).c_str());
             ot = t + 1;
         }
-
-        //printf("\n");
     }
 
-    int count[9] = {0};
-    
-    for(int j=0; j<data.size(); j++){
-        for(int i=0; i<4; i++){
-            switch(data[j][10+i].size()){
-            case 2: count[1]++;break;//1
-            case 4: count[4]++;break;//4
-            case 3: count[7]++;break;//7
-            case 7: count[8]++;break;//8
+
+    vector<string> stuff = {"abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"};
+
+
+
+    unsigned long long answer = 0;
+
+    for(size_t z = 0;z<data.size();z++){
+
+        string map = "abcdefg";    
+        for(size_t j=0;j<5040;j++){
+            string final = " ";
+            for(int i=0;i<10;i++){
+                string tmp = data[z][i];
+                for(int q=0;q<tmp.size();q++){
+                    tmp[q] = map[ tmp[q]-97 ];
+                }
+
+                sort(tmp.begin(),tmp.end());
+
+                final += tmp;
             }
+
+            bool found = true;
+            for(string& s: stuff){
+                found = found && final.find(s) != string::npos;
+            }
+            if(found) {
+                printf("yay\n");
+                break;
+            }
+
+            next_permutation(map.begin(), map.end());
+        }
+        /*
+        for(int i=0;i<10;i++){
+            string tmp = data[z][i];
+            for(int q=0;q<tmp.size();q++){
+                tmp[q] = map[ tmp[q]-97 ];
+            }
+            sort(tmp.begin(),tmp.end());
+
+            printf("%i: %s ", i, tmp.c_str());
+        }
+        printf("\n");*/
+
+        int dec = 1;
+        for(int i=0;i<4;i++){
+            string tmp = data[z][13-i];
+            for(int q=0;q<tmp.size();q++){
+                tmp[q] = map[ tmp[q]-97 ];
+            }
+            sort(tmp.begin(),tmp.end());
+
+            answer += dec*distance(stuff.begin(),find(stuff.begin(), stuff.end(), tmp));
+            dec*=10;
         }
     }
-
-    int sum = count[1] + count[4] + count[7] + count[8];
-    printf("1: %i, 4: %i, 7: %i, 8: %i, sum: %i", count[1], count[4], count[7], count[8], sum);
+    printf("answer: %llu\n",answer);
     //printf("biggest ull: %llu\n", 18446744073709551615ull);
 
 	return 0;
